5fca300b5885c68a84c752ee564120584a4deaf4
Tummeli Ruonakoski <66354966+TummeliRuonakoski@users.noreply.github.com>
2022-02-16 12:52:05 +0200

    cafe angular businessCollege

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cafe';
+  tarjoiluOhje: any = [];
+  // tarjoiluOhje = [{tyo:'myyty', poydanNumero:'24', myyntiMaara:'90'}];
+
+  kahvinMyynti(myyntiData: {poydanNumero:string, myyntiMaara:string}) {
+    this.tarjoiluOhje.push( {
+      tyo: 'myyty',
+      poydanNumero: myyntiData.poydanNumero,
+      myyntiMaara: myyntiData.myyntiMaara
+    })
+  }
+
+  kahvinTarjoilu(tarjoiluData: {poydanNumero:string, myyntiMaara:string}) {
+    this.tarjoiluOhje.push({
+      tyo: 'tarjoiltu',
+      poydanNumero: tarjoiluData.poydanNumero,
+      myyntiMaara: tarjoiluData.myyntiMaara
+    });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { KahvilaComponent } from './kahvila/kahvila.component';
+import { MyyntiComponent } from './myynti/myynti.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KahvilaComponent,
+    MyyntiComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
